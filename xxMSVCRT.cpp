2511819ca67be20c949288b494fdaea3d3d49bbd
TAiGA <@>
2019-10-09 22:42:43 +0800

    MSVCRT : Reduce loading time

--- xxMSVCRT.cpp
+++ xxMSVCRT.cpp
@@ -13,14 +13,18 @@
 #include <float.h>
 #include <setjmp.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <intrin.h>
 #include <limits.h>
 #include <malloc.h>
 #include <math.h>
 
-#pragma comment(lib, "int64")
-
 #ifndef _DEBUG
+
+#if defined(_M_IX86)
+#pragma comment(lib, "int64")
+#endif
+#pragma comment(linker, "/nodefaultlib:libcmt.lib")
 
 //==============================================================================
 //  new / delete
@@ -38,8 +42,6 @@ void operator delete(void* ptr, size_t size)
 //==============================================================================
 extern "C" int _fltused = 1;
 static HMODULE msvcrt = nullptr;
-static HMODULE user32 = nullptr;
-static HMODULE unicows = nullptr;
 //------------------------------------------------------------------------------
 #pragma section(".CRT$XCA", long, read)
 #pragma section(".CRT$XCZ", long, read)
@@ -70,15 +72,11 @@ extern "C" BOOL WINAPI _DllMainCRTStartup(HANDLE handle, DWORD reason, LPVOID pr
     case DLL_PROCESS_ATTACH:
         if (msvcrt == nullptr)
         {
+#if defined(_M_IX86)
             msvcrt = LoadLibraryA("msvcrt20.dll");
-        }
-        if (user32 == nullptr)
-        {
-            user32 = LoadLibraryA("user32.dll");
-        }
-        if (unicows == nullptr)
-        {
-            unicows = LoadLibraryA("unicows.dll");
+#else
+            msvcrt = LoadLibraryA("msvcrt.dll");
+#endif
         }
         _initterm(&__xi_a, &__xi_z);
         _initterm(&__xc_a, &__xc_z);
@@ -89,16 +87,6 @@ extern "C" BOOL WINAPI _DllMainCRTStartup(HANDLE handle, DWORD reason, LPVOID pr
         {
             FreeLibrary(msvcrt);
             msvcrt = nullptr;
-        }
-        if (user32)
-        {
-            FreeLibrary(user32);
-            user32 = nullptr;
-        }
-        if (unicows)
-        {
-            FreeLibrary(unicows);
-            unicows = nullptr;
         }
         break;
 
@@ -113,6 +101,7 @@ static FILE** __iob;
 static int (*__vfprintf)(FILE*, char const*, va_list);
 static int (*__vsnprintf)(char*, size_t, char const*, va_list);
 static int (*__vsscanf)(char const*, char const*, va_list);
+static float (*__sinf)(float);
 //------------------------------------------------------------------------------
 static FILE* acrt_iob_func(unsigned int i)
 {
@@ -134,11 +123,17 @@ static int stdio_common_vsscanf(unsigned __int64 options, char const* buffer, si
     return __vsscanf(buffer, format, argList);
 }
 //------------------------------------------------------------------------------
+static float __fastcall libm_sse2_sincosf(float v)
+{
+    return __sinf(v);
+}
+//------------------------------------------------------------------------------
 static void* aligned_malloc(size_t size, size_t alignment)
 {
     size_t unalignedPtr = reinterpret_cast<size_t>(malloc(size + alignment));
     if (unalignedPtr == 0)
         return nullptr;
+
     char* alignedPtr = (char*)nullptr + ((unalignedPtr + alignment) & ~(alignment - 1));
     memcpy(alignedPtr - sizeof(size_t), &unalignedPtr, sizeof(size_t));
     return alignedPtr;
@@ -149,11 +144,13 @@ static void* aligned_realloc(void* ptr, size_t size, size_t alignment)
     char* alignedPtr = (char*)ptr;
     if (alignedPtr == nullptr)
         return aligned_malloc(size, alignment);
+
     size_t unalignedPtr;
     memcpy(&unalignedPtr, alignedPtr - sizeof(size_t), sizeof(size_t));
     unalignedPtr = reinterpret_cast<size_t>(realloc((char*)nullptr + unalignedPtr, size + alignment));
     if (unalignedPtr == 0)
         return nullptr;
+
     alignedPtr = (char*)nullptr + ((unalignedPtr + alignment) & ~(alignment - 1));
     memcpy(alignedPtr - sizeof(size_t), &unalignedPtr, sizeof(size_t));
     return alignedPtr;
@@ -164,6 +161,7 @@ static void aligned_free(void* ptr)
     char* alignedPtr = (char*)ptr;
     if (alignedPtr == nullptr)
         return;
+
     size_t unalignedPtr;
     memcpy(&unalignedPtr, alignedPtr - sizeof(size_t), sizeof(size_t));
     free((char*)nullptr + unalignedPtr);
@@ -203,6 +201,14 @@ static void* getFunction(const char* name)
         }
         return stdio_common_vsscanf;
     }
+    if (name == "__libm_sse2_sincosf_")
+    {
+        if (__sinf == nullptr)
+        {
+            (void*&)__sinf = getFunction("sinf");
+        }
+        return libm_sse2_sincosf;
+    }
 
     if (name == "_aligned_malloc")
         return aligned_malloc;
@@ -211,11 +217,20 @@ static void* getFunction(const char* name)
     if (name == "_aligned_free")
         return aligned_free;
 
+    if (name == "setjmp")
+        name = "_setjmp";
+
     void* function = nullptr;
 
     function = GetProcAddress(msvcrt, name);
     if (function)
         return function;
+
+    static HMODULE user32 = nullptr;
+    if (user32 == nullptr)
+    {
+        user32 = LoadLibraryA("user32.dll");
+    }
 
     static int (WINAPI * MessageBoxA)(HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType);
     if (MessageBoxA == nullptr)
@@ -228,160 +243,110 @@ static void* getFunction(const char* name)
 }
 //------------------------------------------------------------------------------
 #if defined(_M_IX86)
-#define _IMP_(v) _imp__ ## v
-#elif defined(_M_AMD64)
-#define _IMP_(v) __imp_ ## v
+#define FUNCTION(result, function, parameter, ...) \
+static const char* xx ## function ## name = #function; \
+static result (*xx ## function)(__VA_ARGS__); \
+extern "C" __declspec(naked) result function(__VA_ARGS__) \
+{ \
+    __asm lea   ecx, xx ## function \
+    __asm cmp   dword ptr [ecx], 0 \
+    __asm jz    LOAD \
+    __asm RUN: \
+    __asm jmp   dword ptr [ecx] \
+    __asm LOAD: \
+    __asm push  ecx \
+    __asm push  xx ## function ## name \
+    __asm call  getFunction \
+    __asm pop   ecx \
+    __asm pop   ecx \
+    __asm mov   [ecx], eax \
+    __asm jmp   RUN \
+}
+#else
+#define FUNCTION(result, function, parameter, ...) \
+static const char* xx ## function ## name = #function; \
+static result (*xx ## function)(__VA_ARGS__); \
+extern "C" result function(__VA_ARGS__) \
+{ \
+    if (xx ## function == nullptr) \
+    { \
+        (void*&)xx ## function = getFunction(xx ## function ## name); \
+    } \
+    return xx ## function ## parameter; \
+}
 #endif
-//------------------------------------------------------------------------------
-extern "C" void* _IMP_(atexit)                  = getFunction("atexit");
-extern "C" void* _IMP_(atof)                    = getFunction("atof");
-extern "C" void* _IMP_(ceil)                    = getFunction("ceil");
-extern "C" void* _IMP_(floor)                   = getFunction("floor");
-extern "C" void* _IMP_(calloc)                  = getFunction("calloc");
-extern "C" void* _IMP_(malloc)                  = getFunction("malloc");
-extern "C" void* _IMP_(realloc)                 = getFunction("realloc");
-extern "C" void* _IMP_(free)                    = getFunction("free");
-extern "C" void* _IMP_(_aligned_malloc)         = getFunction("_aligned_malloc");
-extern "C" void* _IMP_(_aligned_realloc)        = getFunction("_aligned_realloc");
-extern "C" void* _IMP_(_aligned_free)           = getFunction("_aligned_free");
-extern "C" void* _IMP_(fclose)                  = getFunction("fclose");
-extern "C" void* _IMP_(fflush)                  = getFunction("fflush");
-extern "C" void* _IMP_(fopen)                   = getFunction("fopen");
-extern "C" void* _IMP_(fread)                   = getFunction("fread");
-extern "C" void* _IMP_(fseek)                   = getFunction("fseek");
-extern "C" void* _IMP_(ftell)                   = getFunction("ftell");
-extern "C" void* _IMP_(fwrite)                  = getFunction("fwrite");
-extern "C" void* _IMP_(getenv)                  = getFunction("getenv");
-extern "C" void* _IMP_(qsort)                   = getFunction("qsort");
-extern "C" void* _IMP_(longjmp)                 = getFunction("longjmp");
-extern "C" void* _IMP_(_setjmp)                 = getFunction("_setjmp");
-extern "C" void* _IMP_(memchr)                  = getFunction("memchr");
-extern "C" void* _IMP_(memcmp)                  = getFunction("memcmp");
-extern "C" void* _IMP_(memcpy)                  = getFunction("memcpy");
-extern "C" void* _IMP_(memmove)                 = getFunction("memmove");
-extern "C" void* _IMP_(memset)                  = getFunction("memset");
-extern "C" void* _IMP_(strchr)                  = getFunction("strchr");
-extern "C" void* _IMP_(strcmp)                  = getFunction("strcmp");
-extern "C" void* _IMP_(strncmp)                 = getFunction("strncmp");
-extern "C" void* _IMP_(strncpy)                 = getFunction("strncpy");
-extern "C" void* _IMP_(strrchr)                 = getFunction("strrchr");
-extern "C" void* _IMP_(strstr)                  = getFunction("strstr");
-extern "C" void* _IMP_(strtol)                  = getFunction("strtol");
-extern "C" void* _IMP_(toupper)                 = getFunction("toupper");
-extern "C" void* _IMP_(_wfopen)                 = getFunction("_wfopen");
-extern "C" void* _IMP_(_CIfmod)                 = getFunction("_CIfmod");
-extern "C" void* _IMP_(_CIacos)                 = getFunction("_CIacos");
-extern "C" void* _IMP_(_CIpow)                  = getFunction("_CIpow");
-extern "C" void* _IMP_(__acrt_iob_func)         = getFunction("__acrt_iob_func");
-extern "C" void* _IMP_(__stdio_common_vfprintf) = getFunction("__stdio_common_vfprintf");
-extern "C" void* _IMP_(__stdio_common_vsprintf) = getFunction("__stdio_common_vsprintf");
-extern "C" void* _IMP_(__stdio_common_vsscanf)  = getFunction("__stdio_common_vsscanf");
-//------------------------------------------------------------------------------
-extern "C" int atexit(void (*f)(void))
-{
-    int (*function)(void (*f)(void));
-    (void*&)function = _IMP_(atexit);
-    return function(f);
-}
-//------------------------------------------------------------------------------
 #pragma function(ceil)
-extern "C" double ceil(double f)
-{
-    double (*function)(double f);
-    (void*&)function = _IMP_(ceil);
-    return function(f);
-}
-//------------------------------------------------------------------------------
 #pragma function(floor)
-extern "C" double floor(double f)
-{
-    double (*function)(double f);
-    (void*&)function = _IMP_(floor);
-    return function(f);
-}
-//------------------------------------------------------------------------------
+#pragma function(pow)
+#if defined(_M_AMD64)
+#pragma function(asinf)
+#pragma function(acosf)
+#pragma function(atan2f)
+#pragma function(sinf)
+#pragma function(cosf)
+#pragma function(powf)
+#pragma function(fmodf)
+#endif
 #pragma function(memchr)
-extern "C" void const* memchr(void const* a, int b, size_t c)
-{
-    void const* (*function)(void const* a, int b, size_t c);
-    (void*&)function = _IMP_(memchr);
-    return function(a, b, c);
-}
-//------------------------------------------------------------------------------
 #pragma function(memcmp)
-extern "C" int memcmp(void const* a, void const* b, size_t c)
-{
-    int (*function)(void const* a, void const* b, size_t c);
-    (void*&)function = _IMP_(memcmp);
-    return function(a, b, c);
-}
-//------------------------------------------------------------------------------
 #pragma function(memcpy)
-extern "C" void* memcpy(void* a, void const* b, size_t c)
-{
-    void* (*function)(void* a, void const* b, size_t c);
-    (void*&)function = _IMP_(memcpy);
-    return function(a, b, c);
-}
-//------------------------------------------------------------------------------
 #pragma function(memmove)
-extern "C" void* memmove(void* a, void const* b, size_t c)
-{
-    void* (*function)(void* a, void const* b, size_t c);
-    (void*&)function = _IMP_(memmove);
-    return function(a, b, c);
-}
-//------------------------------------------------------------------------------
 #pragma function(memset)
-extern "C" void* memset(void* a, int b, size_t c)
-{
-    void* (*function)(void* a, int b, size_t c);
-    (void*&)function = _IMP_(memset);
-    return function(a, b, c);
-}
-//------------------------------------------------------------------------------
 #pragma function(strcmp)
-extern "C" int strcmp(char const* a, char const* b)
-{
-    int (*function)(char const* a, char const* b);
-    (void*&)function = _IMP_(strcmp);
-    return function(a, b);
-}
-//------------------------------------------------------------------------------
-extern "C" char const* strchr(char const* a, int b)
-{
-    char* (*function)(char const* a, int b);
-    (void*&)function = _IMP_(strchr);
-    return function(a, b);
-}
-//------------------------------------------------------------------------------
-extern "C" char const* strrchr(char const* a, int b)
-{
-    char* (*function)(char const* a, int b);
-    (void*&)function = _IMP_(strrchr);
-    return function(a, b);
-}
-//------------------------------------------------------------------------------
-extern "C" char const* strstr(char const* a, char const* b)
-{
-    char const* (*function)(char const* a, char const* b);
-    (void*&)function = _IMP_(strstr);
-    return function(a, b);
-}
-//------------------------------------------------------------------------------
-extern "C" __declspec(noreturn) void longjmp(jmp_buf a, int b) noexcept(false)
-{
-    void (*function)(jmp_buf a, int b);
-    (void*&)function = _IMP_(longjmp);
-    function(a, b);
-}
-//------------------------------------------------------------------------------
-extern "C" int setjmp(jmp_buf a)
-{
-    int (*function)(jmp_buf a);
-    (void*&)function = _IMP_(_setjmp);
-    return function(a);
-}
+FUNCTION(int,           atexit,                     (a),                void (*a)(void));
+FUNCTION(double,        atof,                       (a),                char const* a);
+FUNCTION(double,        ceil,                       (a),                double a);
+FUNCTION(double,        floor,                      (a),                double a);
+FUNCTION(double,        pow,                        (a, b),             double a, double b);
+#if defined(_M_AMD64)
+FUNCTION(float,         asinf,                      (a),                float a);
+FUNCTION(float,         acosf,                      (a),                float a);
+FUNCTION(float,         atan2f,                     (a, b),             float a, float b);
+FUNCTION(float,         sinf,                       (a),                float a);
+FUNCTION(float,         cosf,                       (a),                float a);
+FUNCTION(float,         powf,                       (a, b),             float a, float b);
+FUNCTION(float,         fmodf,                      (a, b),             float a, float b);
+#endif
+FUNCTION(void*,         calloc,                     (a, b),             size_t a, size_t b);
+FUNCTION(void*,         malloc,                     (a),                size_t a);
+FUNCTION(void*,         realloc,                    (a, b),             void* a, size_t b);
+FUNCTION(void,          free,                       (a),                void* a);
+FUNCTION(void*,         _aligned_malloc,            (a, b),             size_t a, size_t b);
+FUNCTION(void*,         _aligned_realloc,           (a, b, c),          void* a, size_t b, size_t c);
+FUNCTION(void,          _aligned_free,              (a),                void* a);
+FUNCTION(int,           fclose,                     (a),                FILE* a);
+FUNCTION(int,           fflush,                     (a),                FILE* a);
+FUNCTION(FILE*,         fopen,                      (a, b),             char const* a, char const* b);
+FUNCTION(size_t,        fread,                      (a, b, c, d),       void* a, size_t b, size_t c, FILE* d);
+FUNCTION(int,           fseek,                      (a, b, c),          FILE* a, long b, int c);
+FUNCTION(long,          ftell,                      (a),                FILE* a);
+FUNCTION(size_t,        fwrite,                     (a, b, c, d),       void const* a, size_t b, size_t c, FILE* d);
+FUNCTION(char*,         getenv,                     (a),                char const* a);
+FUNCTION(void,          qsort,                      (a, b, c, d),       void* a, size_t b, size_t c, _CoreCrtNonSecureSearchSortCompareFunction d);
+FUNCTION(void,          longjmp,                    (a, b),             jmp_buf a, int b);
+FUNCTION(int,           setjmp,                     (a),                jmp_buf a);
+FUNCTION(void const*,   memchr,                     (a, b, c),          void const* a, int b, size_t c);
+FUNCTION(int,           memcmp,                     (a, b, c),          void const* a, void const* b, size_t c);
+FUNCTION(void*,         memcpy,                     (a, b, c),          void* a, void const* b, size_t c);
+FUNCTION(void*,         memmove,                    (a, b, c),          void* a, void const* b, size_t c);
+FUNCTION(void*,         memset,                     (a, b, c),          void* a, int b, size_t c);
+FUNCTION(char const*,   strchr,                     (a, b),             char const* a, int b);
+FUNCTION(int,           strcmp,                     (a, b),             char const* a, char const* b);
+FUNCTION(int,           strncmp,                    (a, b, c),          char const* a, char const* b, size_t c);
+FUNCTION(char*,         strncpy,                    (a, b, c),          char* a, char const* b, size_t c);
+FUNCTION(char const*,   strrchr,                    (a, b),             char const* a, int b);
+FUNCTION(char const*,   strstr,                     (a, b),             char const* a, char const* b);
+FUNCTION(long,          strtol,                     (a, b, c),          char const* a, char** b, int c);
+FUNCTION(int,           toupper,                    (a),                int a);
+FUNCTION(FILE*,         _wfopen,                    (a, b),             wchar_t const* a, wchar_t const* b);
+FUNCTION(void,          _CIfmod,                    ());
+FUNCTION(void,          _CIacos,                    ());
+FUNCTION(void,          _CIpow,                     ());
+FUNCTION(void,          __libm_sse2_sincosf_,       ());
+FUNCTION(FILE*,         __acrt_iob_func,            (a),                unsigned a);
+FUNCTION(int,           __stdio_common_vfprintf,    (a, b, c, d, e),    unsigned __int64 a, FILE* b, char const* c, _locale_t d, va_list e);
+FUNCTION(int,           __stdio_common_vsprintf,    (a, b, c, d, e, f), unsigned __int64 a, char* b, size_t c, char const* d, _locale_t e, va_list f);
+FUNCTION(int,           __stdio_common_vsscanf,     (a, b, c, d, e, f), unsigned __int64 a, char const* b, size_t c, char const* d, _locale_t e, va_list f);
 //------------------------------------------------------------------------------
 #if defined(_M_IX86)
 extern "C" __declspec(naked) void _chkstk()
@@ -392,21 +357,6 @@ extern "C" __declspec(naked) void _chkstk()
     __asm mov       eax, [eax]
     __asm mov       [esp], eax
     __asm ret
-}
-//------------------------------------------------------------------------------
-extern "C" __declspec(naked) void _CIfmod()
-{
-    __asm jmp _IMP_(_CIfmod);
-}
-//------------------------------------------------------------------------------
-extern "C" __declspec(naked) void _CIacos()
-{
-    __asm jmp _IMP_(_CIacos);
-}
-//------------------------------------------------------------------------------
-extern "C" __declspec(naked) void* _CIpow()
-{
-    __asm jmp _IMP_(_CIpow);
 }
 //------------------------------------------------------------------------------
 extern "C" __declspec(naked) void* _ftol2()
@@ -456,6 +406,5 @@ extern "C" void __chkstk()
 {
 }
 #endif
-//------------------------------------------------------------------------------
 
 #endif
